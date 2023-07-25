4548a67e58d41500157a485a667ec2cc7b55a62b
Yonnys Quintero <gladibethdelosa@gmail.com>
2023-07-25 17:36:06 -0500

    feat: :zap: Using the router within the setup

---
+++ src/views/Pokemon.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Single de pokemon</h1>
+</template>
+<script>
+</script>
---
+++ src/views/SearchPokemon.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Buscar un pokémon</h1>
+  <form @submit.prevent="onSubmit">
+    <input 
+    type="number"
+    placeholder="Buscar un pokemon por id"
+    autofocus
+    v-model="pokemonId"
+    >
+  </form>
+  <span>Presione enter para burcar su pokémon</span>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+  export default{
+    setup(){
+      const router = useRouter()
+
+      const pokemonId = ref(1)
+
+      return{
+        pokemonId,
+
+        onSubmit: () => {
+          router.push({name: 'pokemon', params: {id: pokemonId.value}})
+        }
+      }
+    }
+  }
+</script>
